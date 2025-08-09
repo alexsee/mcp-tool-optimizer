d6e243cbfac6cca890742e3e806922cdb397de04
Alexander Seeliger <alexander@brightbits.de>
2025-08-09 23:17:03 +0200

    feat: add basic agent for optimization

---
+++ commands/mcp_tool_optimize.py
@@ -0,0 +1,41 @@
+from typing import Any
+from agents import Agent, Runner
+from agents.mcp.server import MCPServerStreamableHttp, MCPServerStreamableHttpParams
+from agents.mcp import create_static_tool_filter
+
+
+instructions = """
+You are an AI Agent Tool developer can you make the description of a tool more precise and clear.
+The problem is that the LLM sometimes doesn't translate the queries correctly.
+This comes from the fact that the tool description doesn't explain what exactly is allowed and how to use the tool correctly.
+
+How can these issues be avoided by validation i.e. with this tool or external resources or it's just a user mistake? List the main issues and how to avoid them.
+
+Now that the issues and how to avoid them are clear. Please create tool description that addresses these issues. The description is a manual on how to use the tool correctly and what is allowed and what is not.
+It should explain how to avoid the issues that were found in the examples. Add that the tool can be invoked multiple times for better validation of the file system state.
+
+Also give few examples if you think they are applicable.
+
+Please provide description which reflects these issues. The new description shouldn't be longer than 100 words.
+"""
+
+async def mcp_tool_optimize(url: str, tool_name: str, headers: dict[str, str] | None = None) -> Any:
+    # define mcp server
+    mcp_params = MCPServerStreamableHttpParams(
+        url=url,
+        headers=headers,
+    )
+    mcp_server =  MCPServerStreamableHttp(params=mcp_params, tool_filter=create_static_tool_filter(
+        allowed_tool_names=[tool_name]
+    ))
+
+    # define agent
+    agent = Agent(
+        name="MCP Tool Optimizer",
+        description="An agent to optimize MCP tool descriptions.",
+        instructions=instructions,
+        mcp_servers=[mcp_server]
+    )
+
+    result = await Runner.run(agent, "Optimize the tool with the name " + tool_name)
+    return result.final_output
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from mcp-tool-optimizer!")
+import click
+
+
+@click.command()
+@click.argument("url")
+@click.argument("tool_name")
+@click.option("--headers", default=None)
+async def mcp_tool_optimize(url: str, tool_name: str, headers: dict[str, str] | None = None):
+    click.echo("Optimizing MCP tool descriptions...")
+    result = await mcp_tool_optimize(url, tool_name, headers)
+    click.echo(result)
 
 
 if __name__ == "__main__":
-    main()
+    mcp_tool_optimize()
